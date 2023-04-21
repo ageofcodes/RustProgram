098dd3b8006d5a3df9f8abb05fc905959463485e
54544ever <87129537+54544ever@users.noreply.github.com>
2023-04-21 14:14:03 +0200

    Startup

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    println!("Hello, world!");
+    //
+    selector();
+    
+}
+
+fn selector() {
+    fahrenheit_celcius();
+    nth_fibonacci();
+    the_twelve_days_of_xmus();
+}
+
+fn fahrenheit_celcius() {
+// Convert temperatures between Fahrenheit and Celsius.
+println!("Hello, Fahrenheit!");
+} 
+
+fn nth_fibonacci() {
+// Generate the nth Fibonacci number.
+println!("Hello, Fibonacci!");
+} 
+
+fn the_twelve_days_of_xmus() {
+// Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” taking advantage of the repetition in the song. 
+println!("Hello, Carol!");
+} 
